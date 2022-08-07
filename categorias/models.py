3f20fddd6693f92f7bc7cde1be30657a2191c24f
Paula Garrocho <55091571+paugarrocho@users.noreply.github.com>
2022-08-07 18:57:43 -0300

    update categorias

--- categorias/models.py
+++ categorias/models.py
@@ -3,5 +3,4 @@ from django.db import models
 # Create your models here.
 class Categorias(models.Model):
      nombre=models.CharField(max_length=50)
-     id_producto=models.IntegerField()
      clasificacion=models.CharField(max_length=50)
