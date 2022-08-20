f3da5409e6b54a0f95646a24cbb7d80738e8a6fe
Miguel Arce <correo.arce@gmail.com>
2022-08-19 21:30:51 -0300

    borrar-actualizar

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,12 +1,15 @@
 
 from django.urls import path
 from . import views
+from cliente.views import Borrar_cliente
 
 urlpatterns = [
     
     path('lista_clientes/', views.lista_clientes, name="lista_clientes"),
     path('nuevo_cliente/', views.nuevo_cliente, name='nuevo_cliente'),
     path('buscar_cliente/', views.buscar_cliente, name='buscar_cliente'),
+    path('borrar_cliente/<int:pk>/', Borrar_cliente.as_view(), name='borrar_cliente'),
+    path('editar_cliente/<int:pk>/', views.editar_cliente, name='editar_cliente'),
     
 
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from cliente.forms import Formulario_clientes
 from cliente.models import Cliente
+from django.views.generic import  DeleteView
 # Create your views here.
 
 def lista_clientes(request):
@@ -33,4 +34,38 @@ def buscar_cliente(request):
     clientes = Cliente.objects.filter(nombre__icontains=buscar) 
     context = {'clientes':clientes}
     return render(request, 'cliente/buscar_cliente.html', context=context)
+
+
+def editar_cliente(request, pk):
+    if request.method == 'POST':
+        form = Formulario_clientes(request.POST)
+        if form.is_valid():
+            cliente = Cliente.objects.get(id=pk)
+            cliente.nombre = form.cleaned_data['nombre']
+            cliente.direccion = form.cleaned_data['direccion']
+            cliente.telefono = form.cleaned_data['telefono']
+            cliente.email = form.cleaned_data['email']
+            cliente.save()
+
+            return redirect(lista_clientes)
+
+
+    elif request.method == 'GET':
+        cliente = Cliente.objects.get(id=pk)
+
+        form = Formulario_clientes(initial={
+                                        'nombre':cliente.nombre,
+                                        'direccion':cliente.direccion, 
+                                        'telefono':cliente.telefono,
+                                        'email':cliente.email})
+        context = {'form':form}
+        return render(request, 'cliente/editar_cliente.html', context=context)
+
+
+class Borrar_cliente(DeleteView):
+    model = Cliente
+    template_name = 'cliente/borrar_cliente.html'
+    success_url = '/cliente/lista_clientes/'
+
+
 
