45a84ff493aea493029e94db4d66324d017c1ec4
Paula Garrocho <55091571+paugarrocho@users.noreply.github.com>
2022-08-22 11:32:02 -0300

    edicion y borrado de categorias

--- categorias/urls.py
+++ categorias/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     
     path('categoria/', views.lista_categorias, name="lista_categorias"),
-    path('nueva_categoria/', views.nueva_categoria, name='nueva_categoria')
-    
+    path('nueva_categoria/', views.nueva_categoria, name='nueva_categoria'),
+    path('editar_categoria/<int:pk>/', views.editar_categoria, name='editar_categoria'),
+    path('borrar_categoria/<int:pk>/', views.Borrar_categoria.as_view(), name='borrar_categoria'),
 
 ]
--- categorias/views.py
+++ categorias/views.py
@@ -1,6 +1,8 @@
+from calendar import c
 from django.shortcuts import render, redirect
 from categorias.forms import Formulario_categorias
 from categorias.models import Categorias
+from django.views.generic import  DeleteView
 # Create your views here.
 
 def lista_categorias(request):
@@ -26,4 +28,32 @@ def nueva_categoria(request):
         form = Formulario_categorias()
         context = {'form':form}
         return render(request, 'categoria/new_categoria.html', context=context)
+    
+    
+def editar_categoria(request, pk):
+    if request.method == 'POST':
+        form = Formulario_categorias(request.POST)
+        if form.is_valid():
+            categoria= Categorias.objects.get(id=pk)
+            categoria.nombre = form.cleaned_data['nombre']
+            categoria.clasificacion = form.cleaned_data['clasificacion']
+            categoria.save()
+            
+            return redirect(lista_categorias)
+        
+    elif request.method == 'GET':
+        categoria = Categorias.objects.get(id=pk)
+            
+        form = Formulario_categorias(initial={
+                                            'nombre':categoria.nombre,
+                                            'clasificacion':categoria.clasificacion})
+            
+        context = {'form':form}
+        return render(request, 'categoria/editar_categoria.html', context=context)
+
+
+class Borrar_categoria(DeleteView):
+    model = Categorias
+    template_name = 'categoria/borrar_categoria.html'
+    success_url = '/categoria/categoria/'
 
