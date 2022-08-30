b40830ae62275f9b104e12cb369a007c1ecb1ed2
Paula Garrocho <55091571+paugarrocho@users.noreply.github.com>
2022-08-30 08:42:35 -0300

    producto

--- producto/urls.py
+++ producto/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('lista_productos/', views.lista_productos, name="lista_productos"),
     path('nuevo_producto/', views.nuevo_producto, name='nuevo_producto'),
     path('buscar_producto/', views.buscar_producto, name='buscar_producto'),
-     path('borrar_producto/<int:pk>/', views.Borrar_producto.as_view(), name='borrar_producto'),
+    path('borrar_producto/<int:pk>/', views.Borrar_producto.as_view(), name='borrar_producto'),
     path('editar_producto/<int:pk>/', views.editar_producto, name='editar_producto'),
-     path('detalle_producto/', views.detalle_producto, name='detalle_producto'),
+    path('detalle_producto/', views.detalle_producto, name='detalle_producto'),
     
 
 ]
